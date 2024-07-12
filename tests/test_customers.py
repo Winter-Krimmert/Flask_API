3de7ef5d6901b4af0c5f285799998597c6788122
Winter-Krimmert <winterk463@gmail.com>
2024-07-12 10:36:32 -0700

    adding deploy hook in workflow

---
+++ tests/test_customers.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest.mock import MagicMock, patch
+from app import app
+
+
+
+class TestCustomersEndpoint(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        self.app = app.test_client()
+
+    def test_get_customers(self):
+        response = self.app.get('/customers')
+        self.assertEqual(response.status_code, 200)
+        data = response.get_json()
+        self.assertIsInstance(data, list)
+
